3707040480b9c191424e0971643691e694b35471
frkawa <k.f.kntn@gmail.com>
2024-01-14 00:32:44 +0900

    Add current user's articles list page

--- next/src/utils/index.ts
+++ next/src/utils/index.ts
@@ -2,7 +2,14 @@ import axios, { AxiosError, AxiosResponse } from 'axios'
 
 export const fetcher = (url: string) =>
   axios
-    .get(url)
+    .get(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': localStorage.getItem('access-token'),
+        client: localStorage.getItem('client'),
+        uid: localStorage.getItem('uid'),
+      },
+    })
     .then((res: AxiosResponse) => res.data)
     .catch((err: AxiosError) => {
       console.log(err.message)
